3fe3e927d026861325e39dc350a0ff70bb48d321
Tiago Alberto <tiago.alberto@lcg.consulting>
2023-08-27 09:36:39 +0100

    Controllers > Render MVC Controller

---
+++ UmbracoTutorial/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace UmbracoTutorial.Controllers;
+
+// The same name as docType
+public class ProductsController:RenderController
+{
+    public ProductsController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine, umbracoContextAccessor)
+    {
+    }
+
+    public override IActionResult Index()
+    {
+        var currentPage = CurrentPage;
+        return CurrentTemplate(currentPage);
+    }
+}
