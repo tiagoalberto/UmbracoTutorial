3609b170c220963e52d5c8aff282fb8ba4f904ae
Tiago Alberto <tiago.alberto@lcg.consulting>
2023-08-27 09:32:47 +0100

    Controllers > Umbraco API Controller

---
+++ UmbracoTutorial/Controllers/TutorialApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace UmbracoTutorial.Controllers;
+
+public class TutorialApiController:UmbracoApiController
+{
+    // /umbraco/api/tutorialapi/getproducts
+    public IActionResult GetProducts()
+    {
+        var products = new List<SimpleProduct>()
+        {
+            new SimpleProduct("abd", "Product Title"),
+            new SimpleProduct("def", "Product Title 2")
+        };
+
+        return Ok(products);
+    }
+
+    private record SimpleProduct(string id, string title);
+}
