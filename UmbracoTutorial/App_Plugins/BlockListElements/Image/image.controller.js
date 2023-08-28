09056f5aac32cce0397d04cd564df77ff2d92139
Tiago Alberto <tiago.alberto@lcg.consulting>
2023-08-28 16:45:14 +0100

    BlockList Editor

---
+++ UmbracoTutorial/App_Plugins/BlockListElements/Image/image.controller.js
@@ -0,0 +1,34 @@
+﻿(function (){
+    "use strict";
+    
+    function bleImageController($scope, mediaResource, imageUrlGeneratorResource){
+        var vm = this;
+        vm.content = $scope.block.data;
+        vm.imageUrl = "";
+        
+        cropImage();
+        
+        $scope.$watch('block.data', function() {
+            vm.content = $scope.block.data;
+            cropImage();
+        }, true);
+        
+        function cropImage(){
+            if(vm.content.image && vm.content.image.length > 0){
+                
+                mediaResource.getById(vm.content.image[0].mediaKey)
+                    .then((media) => {
+                        imageUrlGeneratorResource.getCropUrl(media.mediaLink, 200, 200)
+                            .then((result) => {
+                                 vm.imageUrl = result;
+                            });
+                    });
+                
+            }else{
+                vm.imageUrl = "";
+            }
+        }
+    }
+    
+    angular.module("umbraco").controller("bleImageController", bleImageController);
+})();
