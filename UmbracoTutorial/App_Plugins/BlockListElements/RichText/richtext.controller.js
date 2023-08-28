bcc6efc3c5fe88141265966d5a76cf756f94be69
Tiago Alberto <tiago.alberto@lcg.consulting>
2023-08-28 17:00:48 +0100

    Block List Editor Settings

---
+++ UmbracoTutorial/App_Plugins/BlockListElements/RichText/richtext.controller.js
@@ -0,0 +1,23 @@
+﻿(function (){
+    "use strict";
+
+    function bleRichtextController($scope){
+      var vm = this;
+      
+      vm.richTextContent = $scope.block.data.richText;
+      
+      //vm.settings = $scope.block?.settingsData;
+      
+      vm.backgroundColor = $scope.block?.settingsData?.backgroundColor?.value; 
+      console.log($scope.block?.settingsData);
+      $scope.$watch('block.data', function (){
+          vm.richTextContent = $scope.block.data.richText;
+      }, true);
+      $scope.$watch('block.settingsData', function (){
+          vm.backgroundColor = $scope.block?.settingsData?.backgroundColor?.value;
+      }, true);
+      
+    }
+
+    angular.module("umbraco").controller("bleRichtextController", bleRichtextController);
+})();
