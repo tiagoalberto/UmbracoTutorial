0655fa7b501d36156b5da49f7fbd3725ba9a7e0c
Tiago Alberto <tiago.alberto@lcg.consulting>
2023-08-28 11:54:10 +0100

    Extend ModelsBuilder Models

---
+++ UmbracoTutorial.Core/Models/Umbraco/Home.cs
@@ -0,0 +1,19 @@
+﻿namespace UmbracoTutorial.Core.UmbracoModels;
+
+public partial class Home
+{
+    public string ShorHeroDescription
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(this.HeroDescription))
+            {
+                return "";
+            }
+            else
+            {
+                return $"{this.HeroDescription.Substring(0, 30)} ...";
+            }
+        }
+    }
+}
