3cee8e7dde5ac64b80bf3b007280bcb37e547a9e
Tiago Alberto <tiago.alberto@lcg.consulting>
2023-08-28 11:32:37 +0100

    Controllers > Umbraco Authorized Controllers

---
+++ UmbracoTutorial/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Common.Filters;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace UmbracoTutorial.Controllers;
+
+[UmbracoMemberAuthorize]
+public class AccountController:SurfaceController
+{
+    public AccountController(
+        IUmbracoContextAccessor umbracoContextAccessor, 
+        IUmbracoDatabaseFactory databaseFactory,
+        ServiceContext services, 
+        AppCaches appCaches, 
+        IProfilingLogger profilingLogger,
+        IPublishedUrlProvider publishedUrlProvider) 
+        : base(umbracoContextAccessor, databaseFactory, services, appCaches, 
+            profilingLogger, publishedUrlProvider)
+    {
+    }
+
+    [HttpGet]
+    public IActionResult GetUserDetails(int userId)
+    {
+        // Delete user account with the provided userId
+        // ...
+
+        return View();
+    }
+}
---
+++ UmbracoTutorial/Controllers/TutorialAuthorizedController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Web.BackOffice.Controllers;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace UmbracoTutorial.Controllers;
+
+//UmbracoAuthorizedController
+//UmbracoAuthorizedApiController
+//UmbracoAuthorizedJsonController
+
+//Backoffice Authorization
+
+public class TutorialAuthorizedController:UmbracoAuthorizedController
+{
+    
+}
+
+public class TutorialAuthorizedApiController:UmbracoAuthorizedApiController
+{
+    
+}
+
+public class TutorialAuthorizedJsonController:UmbracoAuthorizedJsonController
+{
+    
+}
